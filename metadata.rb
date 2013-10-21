38689d29dbb0d50301e65a8bf31c5a6563044785
Wil Reichert <wil.reichert@kt.com>
2013-10-21 10:13:59 +0900

    fix metadata

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,10 @@
-maintainer       "KT Cloudware"
-description      "Installs Galera Cluster for MySQL"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.5.1"
-recipe "server", "Installs Galera Cluster for MySQL"
+name              "galera"
+maintainer        "KT Cloudware"
+maintainer_email  "wil.reichert@kt.com"
+license           "All rights reserved"
+description       "Installs/Configures Openstack Identity Service"
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version           "0.5.1"
 
 %w{ debian ubuntu centos fedora redhat }.each do |os|
   supports os
